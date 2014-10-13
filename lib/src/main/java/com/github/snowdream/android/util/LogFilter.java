d23216334e42bd504ee62e31e51cf62506b1e3d1
snowdream <yanghui1986527@gmail.com>
2014-10-14 04:50:20 +0800

    Added: log filter

---
+++ lib/src/main/java/com/github/snowdream/android/util/LogFilter.java
@@ -0,0 +1,10 @@
+package com.github.snowdream.android.util;
+
+/**
+ * Created by hui.yang on 2014/10/14.
+ */
+public class LogFilter {
+    public boolean filter(String str){
+        return false;
+    }
+}
---
+++ lib/src/main/java/com/github/snowdream/android/util/LogLevelFilter.java
@@ -0,0 +1,7 @@
+package com.github.snowdream.android.util;
+
+/**
+ * Created by hui.yang on 2014/10/14.
+ */
+public class LogLevelFilter extends LogFilter {
+}
